34d44f1e9dc48d18d2a4a62afef3499eb2839d73
TheOpti <wieckowski.piotr.93@gmail.com>
2021-06-13 18:28:49 +0200

    First commit - structure of website

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('normalize.css/normalize.css');
+require('./styles/index.scss');
+require('./styles/about-company.scss');
+require('./styles/contact.scss');
+require('./styles/footer.scss');
+require('./styles/get-started.scss');
+require('./styles/nav.scss');
+require('./styles/our-projects.scss');
+require('./styles/pricing.scss');
+require('./styles/welcome.scss');
+require('./styles/what-we-offer.scss');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Page loaded');
+});
