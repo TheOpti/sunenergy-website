6c9f0fb0e948c865c74a0cadd167c263899ba296
TheOpti <wieckowski.piotr.93@gmail.com>
2021-06-29 22:01:49 +0200

    Fixes - nav, contact, Google Map

--- src/index.js
+++ src/index.js
@@ -14,33 +14,24 @@ require('./styles/what-we-offer.scss');
 const NAVBAR_HEIGHT = 200;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const navBar = document.querySelector('#nav-bar');
-  const navBarSticky = document.querySelector('#nav-bar-sticky');
+  const navBar = document.querySelector('#navbar');
+  const navBarSticky = document.querySelector('#navbar-sticky');
   const mobileMenuToggleBtn = document.querySelector('#menu-toggle-btn');
   const mobileMenu = document.querySelector('.navigation__menu-mobile');
   const formLoader = document.querySelector('.contact__loader-wrapper');
   const submitFormBtn = document.querySelector('#submit-btn');
   const loaderInfoBox = document.querySelector('.contact__info-box');
 
-  setTimeout(() => {
-    init();
-    initMap();
-  }, 100);
+  document.addEventListener('scroll', changeNavBar);
+  navBar.addEventListener('click', handleNavbarClick);
+  navBarSticky.addEventListener('click', handleNavbarClick);
+  submitFormBtn.addEventListener('click', submitForm);
 
-  function init() {
-    navBarSticky.classList.add('navigation--theme-fixed');
+  mobileMenuToggleBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('navigation__menu-mobile--hidden');
+  });
 
-    document.addEventListener('scroll', changeNavBar);
-    navBar.addEventListener('click', handleNavbarClick);
-    navBarSticky.addEventListener('click', handleNavbarClick);
-    submitFormBtn.addEventListener('click', submitForm);
-
-    mobileMenuToggleBtn.addEventListener('click', () => {
-      mobileMenu.classList.toggle('navigation__menu-mobile--hidden');
-    });
-
-    changeNavBar();
-  }
+  changeNavBar();
 
   function handleNavbarClick(event) {
     if (event.target.classList.contains('navigation__menu-item')) {
@@ -72,13 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailValue = document.querySelector('#email').value;
     const topicValue = document.querySelector('#subject').value;
     const messageValue = document.querySelector('#message').value;
-
     formLoader.classList.toggle('contact__loader-wrapper--active');
-
-    console.log('PRZED WYSLANIEM FORMULARZA, nameValue', nameValue);
-    console.log('PRZED WYSLANIEM FORMULARZA, emailValue', emailValue);
-    console.log('PRZED WYSLANIEM FORMULARZA, topicValue', topicValue);
-    console.log('PRZED WYSLANIEM FORMULARZA, messageValue', messageValue);
 
     fetch('https://submit-form.com/yxbIYTFL', {
       method: 'POST',
@@ -103,8 +88,7 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   }
 
-  // Slider installation and config
-  new Splide( '#image-slider', {
+  new Splide('#image-slider', {
     rewind: true,
     pagination: true,
     cover: true,
@@ -120,50 +104,4 @@ document.addEventListener('DOMContentLoaded', () => {
       },
     }
   }).mount();
-
-  function initMap() {
-    const warsaw = { lat: 52.20722, lng: 20.86833 };
-    const chrusty = { lat: 51.72599, lng: 20.20336 };
-
-    const firstMap = new google.maps.Map(document.getElementById('map-first'), { zoom: 12, center: warsaw });
-    const secondMap = new google.maps.Map(document.getElementById('map-second'), { zoom: 12, center: chrusty });
-
-    const warsawInfowindow = new google.maps.InfoWindow({
-      content: `<div class="maps__marker">
-         <p>Konotopska 9/12</p>
-         <p>02-496 Warszawa</p>
-      </div>`,
-    });
-
-    const chrustyInfowindow = new google.maps.InfoWindow({
-      content: `<div class="maps__marker">
-         <p>Chrusty 66A</p>
-         <p>96-200 Rawa Mazowiecka</p>
-      </div>`,
-    });
-
-    const warsawMarker = new google.maps.Marker({
-      position: warsaw,
-      map: firstMap,
-      // icon: 'src/assets/images/map-marker.svg',
-    });
-
-    const chrustyMarker = new google.maps.Marker({
-      position: chrusty,
-      map: secondMap,
-      // icon: 'src/assets/images/map-marker.svg',
-    });
-
-    warsawInfowindow.open({
-      anchor: warsawMarker,
-      firstMap,
-      shouldFocus: false,
-    });
-
-    chrustyInfowindow.open({
-      anchor: chrustyMarker,
-      secondMap,
-      shouldFocus: false,
-    })
-  }
 });
